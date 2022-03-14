917df0fe1aed2b01d88e65adaee42f4f795c104d
Sarvar55 <sarvar.musazade@gmail.com>
2022-03-14 17:41:55 +0300

    service katmaninda guncellenmeler yapildi ve Optional hale getirilidi

--- src/main/java/com/example/employeemanagment/business/abstratcs/StudentServices.java
+++ src/main/java/com/example/employeemanagment/business/abstratcs/StudentServices.java
@@ -1,11 +1,27 @@
-package com.example.employeemanagment.business.concretes;
+package com.example.employeemanagment.business.abstratcs;
 
 import com.example.employeemanagment.DataAcces.abstracts.StudentRepository;
+import com.example.employeemanagment.entity.Student;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface StudentServices {
+    Optional<Student> getByStudentId(Integer student_id);
 
+    List<Student> findByOrderByRegistationDate();
+
+    Student createStudent(Student student);
+
+    void delete(Student student);
+
+    List<Student> findAll();
+
+    List<Student> getByStudentNameContains(String student_name);
+
+    Optional<List<Student>> getByStudentName(String student_name);
 
 
 }
