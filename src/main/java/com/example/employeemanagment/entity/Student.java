73567c72716eb4bce6e741a3b332089bbd2e0f4f
Sarvar55 <sarvar.musazade@gmail.com>
2022-03-14 17:40:18 +0300

    entity katmai taamlandi

--- src/main/java/com/example/employeemanagment/entity/Student.java
+++ src/main/java/com/example/employeemanagment/entity/Student.java
@@ -1,2 +1,33 @@
-package com.example.employeemanagment.entity;public class Student {
+package com.example.employeemanagment.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "students")
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int studentId;
+
+    @Column(name = "student_name")
+    private String studentName;
+
+    @Column(name = "student_last_name")
+    private String studentLastName;
+
+    @Column(name = "student_class_name")
+    private String studentClassName;
+
+    @Column(name = "registation_date")
+    private Date registationDate;
+
 }
